463ecb70e8e3f10f1d9cdfa0d879ec18808ff669
dylan <dylanabozarth@gmail.com>
2021-03-20 20:31:56 -0700

    undefined

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 let input = document.getElementById('input')
+let output = document.getElementById('span')
 let test = () => {
-    console.log(input.value)
+    output.innerHTML = input.value
+};
+function Email() {
+    let returnString = '',
+    var i = 0;
+for (i = 0; i < input.length; i++) {
+ returnString.append('*');
 }
+output.innerHTML = returnString
+};
