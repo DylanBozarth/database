d1bb509a6946286600ce596f9001902354998215
dylan <dylanabozarth@gmail.com>
2021-03-20 20:40:14 -0700

    I hate javascript It's awesome

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let input = document.getElementById('input')
 let output = document.getElementById('span')
 let test = () => {
-    output.innerHTML = input.value
+   console.log('aa')
 };
 function Email() {
-    let returnString = '',
-    var i = 0;
+    let returnString = ''
+    let i = 0;
 for (i = 0; i < input.length; i++) {
  returnString.append('*');
 }
 output.innerHTML = returnString
 };
+var otherTest = function() {
+    console.log('aa')
+}
